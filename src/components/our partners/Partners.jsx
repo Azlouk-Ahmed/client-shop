439904eef1a58db2170b3b2de400819e786cc684
azlouk96 <ahmedazlouk9@gmail.com>
2024-12-15 18:58:49 +0100

    l3sal

---
+++ src/components/our partners/Partners.jsx
@@ -0,0 +1,68 @@
+import "./styles.css";
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+function Partners() {
+  const imageSources = [
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+  ];
+
+  return (
+    <div className="df-c items-center !gap-12 w-full partners">
+      <div className="title pr"><div className="wave"></div>شركاؤنا</div>
+      <div className="our-part-cont">
+        <Swiper
+             spaceBetween={30}
+             centeredSlides={true}
+             speed={1000}
+             autoplay={{
+             delay: 2000,
+             disableOnInteraction: false,
+             reverseDirection: true,
+             }}
+             pagination={{
+             clickable: true,
+             }}
+             slidesPerView={5}
+             dir="rtl"
+             loop={true}
+             modules={[Autoplay]}
+             className="mySwiper"
+             breakpoints={{
+                0: {
+                  slidesPerView: 2, // 2 slides per view for screens smaller than 600px
+                  spaceBetween: 10,
+                },
+                600: {
+                  slidesPerView: 5, // 5 slides per view for screens larger than 600px
+                  spaceBetween: 30,
+                },
+              }}
+        >
+            {imageSources.map((src, index) => (
+            <SwiperSlide key={index}>
+                <img src={src} alt={`Partner ${index + 1}`} height="100" width="250" />
+            </SwiperSlide>
+            ))}
+        </Swiper>
+
+      </div>
+    </div>
+  );
+}
+
+export default Partners;
--- src/pages/home page/Home.jsx
+++ src/pages/home page/Home.jsx
@@ -3,6 +3,7 @@ import Features from '../../components/features/Features'
 import FillJar from '../../components/fill/FillJar'
 import Gift from '../../components/gifts/Gift'
 import Hero from '../../components/hero/Hero'
+import Partners from '../../components/our partners/Partners'
 import About from '../../components/ourshop/About'
 import Products from '../../components/products/Products'
 import TopSelling from '../../components/top-sellings/TopSelling'
@@ -19,6 +20,7 @@ function Home() {
         <About />
         <TopSelling />
         <AdSection />
+        <Partners />
     </div>
   )
 }
